bf25f96a865d9954eddd99195b61946b86314f13
sadxxface <120758081+sadxxface@users.noreply.github.com>
2023-10-28 08:32:27 +0000

    updated class file and introduced new functions

--- src/components/home/Classes/character.ts
+++ src/components/home/Classes/character.ts
@@ -40,31 +40,34 @@ class Character {
         }
     }
 
-    animation(context: CanvasRenderingContext2D){
-        
-    }
-
 }
+
+var characterArray = []
 
 export function drawCharacter(context:CanvasRenderingContext2D, posX: number, posY: number){
 
     // draw the character, initialize fall, initialize character
 
-    let character = new Character(posX, posY)
+    const character = new Character(posX, posY)
     character.draw(context)
     
-    let updateChar = function () {
-        requestAnimationFrame(updateChar)
+    characterArray.push(character)
+
+    const updateCharacter = function (){
         character.update(context)
     }
-
-    updateChar();
+    requestAnimationFrame(updateCharacter)
 }
 
 export function clickHandler(clickCount:number,posX: number, posY: number, bg:CanvasRenderingContext2D, l1:CanvasRenderingContext2D, l2:CanvasRenderingContext2D, l3:CanvasRenderingContext2D, l4:CanvasRenderingContext2D){
     // do things when the click event happens
+    drawCharacter(l4, posX, posY)
 }
 
-export function loop() {
+export function loop(characterArray:Character[]) {
     // do animation loop
+
+    for (var i in characterArray) {
+        console.log('update character')
+    }
 }
