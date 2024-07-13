5c5bc6782a2e59296a8bba5178330d971224b66d
sadxxface <01191995a@gmail.com>
2024-07-13 01:18:53 -0700

    footer content and styling

--- src/components/template/footer.tsx
+++ src/components/template/footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer(){
-    return(
+    return (
         <footer>
-            <div className="container">
-                Footer go here
+            <div className="copyright">
+                <div className="container">
+                    Copyright 2024 Jay Cumming
+                </div>
             </div>
         </footer>
     )
